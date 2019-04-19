e357f955fbadc0f98cf153683ced2d1e9fc916d1
Jerry Yt Chen (RD-TW) <jerry_yt_chen@trendmicro.com>
2019-04-19 10:28:01 +0800

    [feat] Add task management API

--- api.py
+++ api.py
@@ -1,41 +1,47 @@
-from flask import jsonify
+import json
+
+from flask import jsonify, request
 from flask_restplus import Resource
 
-from moduleA import tasks
+from crawler.tasks import parse_web_links
+from file.tasks import get_python_package, get_multi_python_packages
+
+tasks = {
+    'GetPythonPackage': get_python_package,
+    'GetMultiPythonPackage': get_multi_python_packages,
+    'ParseWebLinks': parse_web_links
+}
 
 
 class Task(Resource):
     def post(self):
-        task = tasks.hello.apply_async()
-        return jsonify({'task_id': task.id})
+        task, kwargs = parse_task(request.data)
+        info = task.apply_async(kwargs=kwargs)
+        return jsonify({'info': info.id})
+
+
+def parse_task(req_data):
+    body = json.loads(req_data)
+    task = tasks.get(body['task'])
+    kwargs = body['meta']
+    return task, kwargs
 
 
 class TaskStatus(Resource):
     def get(self, task_id):
-        task = tasks.hello.AsyncResult(task_id)
+        task = tasks.parse_web_link.AsyncResult(task_id)
         if task.state == 'PENDING':
             response = {
-                'state': task.state,
-                'current': 0,
-                'total': 1,
-                'status': 'Pending...'
+                'state': task.state
             }
         elif task.state != 'FAILURE':
             response = {
                 'state': task.state,
-                'current': task.info.get('current', 0),
-                'total': task.info.get('total', 1),
-                'status': task.info.get('status', ''),
                 'result': task.get()
             }
-            if 'result' in task.info:
-                response['result'] = task.info['result']
         else:
             # something went wrong in the background job
             response = {
                 'state': task.state,
-                'current': 1,
-                'total': 1,
-                'status': str(task.info),  # this is the exception raised
             }
         return jsonify(response)
